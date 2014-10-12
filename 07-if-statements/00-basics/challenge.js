9b5e3d152ca4ad32b2665ea198fe1a8959ccdb1c
Bridgette <bridgettekincaid@gmail.com>
2014-10-12 14:21:19 -0500

    completed exercise 7

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,6 @@
-module.exports.favoriteNumber = undefined;
-
+module.exports.favoriteNumber = (x,y); return x,y
+if (favoriteNumber (5,11) == "Too high") {guess > fav}
+else {if(favoriteNumber(10,1) =="Too low")return{"you got it"== fav};}
 module.exports.checkLock = undefined;
 
 module.exports.canIGet = undefined;
