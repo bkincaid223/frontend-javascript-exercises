bd8af284a40ae19cfe90cfb83bd7823b5aae09c7
Bridgette <bridgettekincaid@gmail.com>
2014-10-12 14:20:40 -0500

    completed exercise 5

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,16 +1,25 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
+return 'stringTwo' != "stringTwo"; "stringTwo" == stringOne;
+ 'bloc' != 'bloc' ;
+
 
 };
 
 module.exports.notEqual = function(one, two) {
+return 'bloc' != 'bloc' + 'bloc' === 'bloc'; 
+
 
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
+var middle = 1;
+return 3 < middle && middle < 5;
+
 
 };
 
 module.exports.outsideRanges = function(number) {
+
 
 };
 
