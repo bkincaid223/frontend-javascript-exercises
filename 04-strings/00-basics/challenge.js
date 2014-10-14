819424d097aab975bf342d3d73d3ca7df7d0bbd8
Bridgette <bridgettekincaid@gmail.com>
2014-10-14 01:21:30 -0500

    completed exercise 4

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,24 +1,23 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-return "Hello Rob,\n\nTake care.\n\nSincerely,\nJoe";
-};  
+	return "Hello Rob,\n\nTake care.\n\nSincerely,\nJoe";
+};
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-var origString = "This is a long test of the emergency broadcast system."
-var bigString = null;
-return "long emergency";
-return "This";
+	return bigString.substring(startA, endA) + bigString.substring(startB, endB);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-return findFirstMatch = (10,0);
-"Roses are red.".indexOf("red", 9);
+	return text.indexOf(searchString);
+	
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-return firstMatch=(10,12);
+	return text.lastIndexOf(searchString);
 
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-return substringBetweenMatches = " red red red red ";
-};
+	firstMatchPosition = text.indexOf(searchString) + searchString.length;
+	lastMatchPosition = text.lastIndexOf(searchString);
+	return text.substring(firstMatchPosition, lastMatchPosition);
+}; 
