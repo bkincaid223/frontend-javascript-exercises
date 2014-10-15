e1e7a0ef39ba80db567d5c4ef745f36eed4bce71
Bridgette <bridgettekincaid@gmail.com>
2014-10-15 18:47:09 -0500

    completed exercise 7

--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,6 +1,49 @@
-module.exports.favoriteNumber = (x,y); return x,y
-if (favoriteNumber (5,11) == "Too high") {guess > fav}
-else {if(favoriteNumber(10,1) =="Too low")return{"you got it"== fav};}
-module.exports.checkLock = undefined;
+module.exports.favoriteNumber = function(favoriteNumber, guess){
+if (guess > favoriteNumber)
+  {return "Too high";}
 
-module.exports.canIGet = undefined;
+  else if (guess < favoriteNumber)
+  {return "Too low";}
+
+  else (guess == favoriteNumber)
+    {return "You got it!";}
+
+  
+};
+
+
+
+module.exports.checkLock = function(a,b,c,d){
+if (a!= 3 && a!= 5 && a!= 7)
+  {return "incorrect";}
+
+else if (b != 2)
+{return "incorrect";}
+
+
+else if (c < 5 || c > 100)
+  {return "incorrect";}
+
+else if (d >= 9 && d <= 20)
+  {return "incorrect";}
+
+else 
+  {return "correct";}
+};
+
+module.exports.canIGet = function(item,money){
+
+  if (item == 'MacBook Air' && money >= 999)
+    {return true;}
+
+  else if (item == 'MacBook Pro' && money >= 1299)
+    {return true;}
+
+  else if (item == 'Mac Pro' && money >= 2499)
+    {return true;}
+
+  else if (item == 'Apple Sticker' && money >= 1)
+    {return true;}
+  else
+      {return false;}
+  };
