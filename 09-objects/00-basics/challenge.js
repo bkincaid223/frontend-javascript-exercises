c54dcc774b1bbcc3f04a9a8583408af26abe8092
Bridgette <bridgettekincaid@gmail.com>
2014-10-17 01:41:58 -0500

    completed exercise 9

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,32 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle,courseDuration,courseStudents){
 
-module.exports.addProperty = undefined;
+var course = {
+  title: courseTitle,
+  duration: courseDuration,
+  students: courseStudents
+}
+return course
+};
 
-module.exports.formLetter = undefined;
+module.exports.addProperty = function(object,newProp,newValue){
+  if (object[newProp] === undefined)
+  {object[newProp] = newValue}
+return object
+};
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(letter){
+  return "Hello " + letter.recipient + ",\n\n" +letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
+
+module.exports.canIGet = function(item, money){
+if (item == 'MacBook Air' && money >= 999)
+  {return true;}
+else if (item == 'MacBook Pro' && money >=  1299)
+  {return true;}
+else if (item == 'Mac Pro' && money >= 2499)
+  {return true;}
+else if (item == 'Apple Sticker' && money >= 1)
+  {return true;}
+else
+  {return false;}
+};
